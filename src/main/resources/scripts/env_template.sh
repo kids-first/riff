89437b9fa9ca14ce2e0011bd029b3f4d588aca0f
Céline Pelletier <celinepelletier@CRHSJ106784.local>
2021-07-13 12:50:49 -0400

    SKFP-14: Use Keycloak instead of Ego to validate token. Also do some clean-up and add integration tests

--- src/main/resources/scripts/env_template.sh
+++ src/main/resources/scripts/env_template.sh
@@ -19,15 +19,3 @@ export RIFF_DB_PASS=
 # === App Server Config
 export RIFF_ACTIVE_PROFILES="default"
 export RIFF_SERVER_PORT=8081
-
-# === VAULT CONFIG
-# Leave all below empty if not using VAULT
-export VAULT_APPLICATION_NAME=development/overture/riff
-export RIFF_VAULT_URI=
-export RIFF_VAULT_SCHEME=http
-export RIFF_VAULT_HOST=localhost
-export RIFF_VAULT_PORT=8200
-#leave IAM Role blank if using Token authentication
-export RIFF_IAM_ROLE=
-#leave Token blank if using IAM Role
-export VAULT_TOKEN=
