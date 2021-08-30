420c64f242d2f520e24b42be1289b64c2986493d
Céline Pelletier <celinepelletier@CRHSJ106784.local>
2021-08-30 10:57:22 -0400

    SKFP-14: Fix CorsFilter

--- src/main/java/bio/overture/riff/config/WebConfiguration.java
+++ src/main/java/bio/overture/riff/config/WebConfiguration.java
@@ -1,12 +1,16 @@
 package bio.overture.riff.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @Slf4j
 @Configuration
@@ -19,13 +23,27 @@ public class WebConfiguration {
     }
 
     @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
+    @Bean
     public CorsFilter corsFilter(){
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
-        String allowedDomains = env.getProperty("application.cors_allowed_domains", "*");
+        String allowedDomains = env.getProperty("spring.cors_allowed_domains", "*");
 
         log.debug("Allow domains : " + allowedDomains);
 
